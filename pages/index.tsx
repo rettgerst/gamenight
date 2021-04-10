7875f6cfb710810c500dc2395c8bdea2776f675e
rett gerst <tricagamma@gmail.com>
2021-04-10 18:19:00 -0400

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,63 @@
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import head from "next/head";
+
+import ownedGames from "../games.json";
+
+import styles from "./index.module.scss";
+
+import SteamAPI from "steamapi";
+import { GameDetail } from "../types";
+
+export async function getStaticProps(ctx: GetStaticPropsContext) {
+  const { STEAM_WEB_KEY } = process.env;
+
+  if (!STEAM_WEB_KEY)
+    throw new Error("Please add a STEAM_WEB_KEY environment variable");
+
+  const steam = new SteamAPI(STEAM_WEB_KEY);
+
+  const gameDetails = await await Promise.all(
+    ownedGames.map((game) => steam.getGameDetails(game) as Promise<GameDetail>)
+  );
+
+  const games = gameDetails
+    .map((g) => g)
+    .reduce(
+      (map, game) => ({ ...map, [game.steam_appid]: game }),
+      {} as { [key: string]: GameDetail }
+    );
+
+  return { props: { games } };
+}
+
+type HomeProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+function GameCard({ game }: { game: GameDetail }) {
+  return (
+    <div className={styles.GameCard}>
+      <img className={styles.GameImage} src={game.header_image} />
+      <span className={styles.GameName}>{game.name}</span>
+    </div>
+  );
+}
+
+export default function Home({ games }: HomeProps) {
+  return (
+    <>
+      <div className={styles.Content}>
+        <img className={styles.Header} src="https://i.imgur.com/D00VVbQ.png" />
+        <h2>
+          <i>Hosted by @nullhund</i>
+        </h2>
+        <h2>Game Menu</h2>
+        <ul className={styles.GameList}>
+          {Object.entries(games).map(([appId, gameData]) => (
+            <li key={appId}>
+              <GameCard game={gameData} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
---
+++ types.ts
@@ -0,0 +1,96 @@
+export interface PackageGroup {
+  name: string;
+  title: string;
+  description: string;
+  selection_text: string;
+  save_text: string;
+  display_type: number;
+  is_recurring_subscription: string;
+  subs: [
+    {
+      packageid: number;
+      percent_savings_text: string;
+      percent_savings: number;
+      option_text: string;
+      option_description: string;
+      can_get_free_license: string;
+      is_free_license: boolean;
+      price_in_cents_with_discount: number;
+    }
+  ];
+}
+
+export interface GameDetail {
+  type: string;
+  name: string;
+  steam_appid: number;
+  required_age: number;
+  is_free: false;
+  controller_support: string;
+  dlc: number[];
+  detailed_description: string;
+  about_the_game: string;
+  short_description: string;
+  supported_languages: string;
+  reviews: string;
+  header_image: string;
+  website: string;
+  pc_requirements: {
+    minimum: string;
+  };
+  mac_requirements: {
+    minimum: string;
+  };
+  linux_requirements: {
+    minimum: string;
+    recommended: string;
+  };
+  legal_notice: string;
+  developers: string[];
+  publishers: string[];
+  price_overview: {
+    currency: string;
+    initial: number;
+    final: number;
+    discount_percent: number;
+    initial_formatted: string;
+    final_formatted: string;
+  };
+  packages: number[];
+  package_groups: PackageGroup[];
+  platforms: { windows: true; mac: true; linux: true };
+  categories: Array<{ id: number; description: string }>;
+  genres: Array<{ id: string; description: string }>;
+  screenshots: Array<{ id: number; path_thumbnail: string; path_full: string }>;
+  movies: [
+    Array<{
+      id: number;
+      name: string;
+      thumbnail: string;
+      webm: {
+        "480": string;
+        max: string;
+      };
+      mp4: {
+        "480": string;
+        max: string;
+      };
+      highlight: true;
+    }>
+  ];
+  recommendations: { total: number };
+  achievements: {
+    total: number;
+    highlighted: Array<{
+      name: string;
+      path: string;
+    }>;
+  };
+  release_date: { coming_soon: boolean; date: string };
+  support_info: {
+    url: string;
+    email: string;
+  };
+  background: string;
+  content_descriptors: { ids: []; notes: null };
+}
