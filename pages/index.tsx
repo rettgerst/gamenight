59b027a54ed4c6408a2fc9291231a2609f194e1a
rett gerst <tricagamma@gmail.com>
2021-04-14 15:26:15 -0400

    add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
-import Image from 'next/image';
 
 import ownedGames from '../games.json';
 
@@ -8,17 +7,19 @@ import styles from './index.module.scss';
 import SteamAPI from 'steamapi';
 import { GameDescriptor, GameDetail } from '../types';
 import { GameCard } from '../components/GameCard';
-import useWinner from '../hooks/use-winner';
-import { useCallback, useMemo } from 'react';
-import useMyVote from '../hooks/use-my-vote';
-import RelativeTime from '../components/RelativeTime';
-import { voteLifespan } from '../constants';
 import VoteContext from '../contexts/VoteContext';
 import Winner from '../components/Winner';
 import MyVote from '../components/MyVote';
 import VoteExpiration from '../components/Expiration';
+import React from 'react';
+import { NextSeo } from 'next-seo';
 
 export async function getStaticProps(ctx: GetStaticPropsContext) {
+	const { VERCEL_URL } = process.env;
+	const absoluteUrl = VERCEL_URL
+		? `https://${VERCEL_URL}`
+		: 'http://localhost:3000';
+
 	const { STEAM_WEB_KEY } = process.env;
 
 	if (!STEAM_WEB_KEY)
@@ -47,70 +48,75 @@ export async function getStaticProps(ctx: GetStaticPropsContext) {
 		}
 	);
 
-	return { props: { games } };
+	return { props: { games, absoluteUrl } };
 }
 
 type HomeProps = InferGetStaticPropsType<typeof getStaticProps>;
 
-function hasSubgame(input: GameDescriptor): boolean {
+export default function Home({ games, absoluteUrl }: HomeProps) {
 	return (
-		'subGameId' in input &&
-		input.subGameId !== null &&
-		input.subGameId !== undefined
-	);
-}
-
-export default function Home({ games }: HomeProps) {
-	return (
-		<VoteContext.Provider value={{ games: games as any }}>
-			<div className={styles.Content}>
-				<h1 className={styles.Header}>Party Game Night!</h1>
-				<div className={styles.VoteSection}>
-					<Winner />
-					<div className={styles.Spacer} />
-					<MyVote />
-				</div>
-				<VoteExpiration className={styles.VoteExpiration} />
-				<h2 className={styles.AvailableGames}>Available Games</h2>
-				<div className={styles.GameList}>
-					{Object.entries(games).map(([appId, gameData]) => (
-						<GameCard
-							key={appId}
-							game={gameData.steamInfo}
-							subGames={gameData.metadata.subGames}
-						/>
-					))}
-				</div>
-				<div className={styles.Footer}>
-					<span>
-						Built with{' '}
-						<a href="https://nextjs.org">
-							<img
-								className={styles.FooterLogo}
-								src="/nextjs.png"
+		<>
+			<NextSeo
+				title="PGN"
+				description="Party game night game selection and voting!"
+				openGraph={{
+					images: [
+						{
+							url: `${absoluteUrl}/opengraph-image.png`
+						}
+					]
+				}}
+			/>
+			<VoteContext.Provider value={{ games: games as any }}>
+				<div className={styles.Content}>
+					<h1 className={styles.Header}>Party Game Night!</h1>
+					<div className={styles.VoteSection}>
+						<Winner />
+						<div className={styles.Spacer} />
+						<MyVote />
+					</div>
+					<VoteExpiration className={styles.VoteExpiration} />
+					<h2 className={styles.AvailableGames}>Available Games</h2>
+					<div className={styles.GameList}>
+						{Object.entries(games).map(([appId, gameData]) => (
+							<GameCard
+								key={appId}
+								game={gameData.steamInfo}
+								subGames={gameData.metadata.subGames}
 							/>
-						</a>
-					</span>
-					<span>
-						Hosted on{' '}
-						<a href="https://vercel.app">
-							<img
-								className={styles.FooterLogo}
-								src="/vercel.png"
-							/>
-						</a>
-					</span>
-					<span>
-						Data from{' '}
-						<a href="https://store.steampowered.com">
-							<img
-								className={`${styles.FooterLogo} ${styles.Invert}`}
-								src="/steam.png"
-							/>
-						</a>
-					</span>
+						))}
+					</div>
+					<div className={styles.Footer}>
+						<span>
+							Built with{' '}
+							<a href="https://nextjs.org">
+								<img
+									className={styles.FooterLogo}
+									src="/nextjs.png"
+								/>
+							</a>
+						</span>
+						<span>
+							Hosted on{' '}
+							<a href="https://vercel.app">
+								<img
+									className={styles.FooterLogo}
+									src="/vercel.png"
+								/>
+							</a>
+						</span>
+						<span>
+							Data from{' '}
+							<a href="https://store.steampowered.com">
+								<img
+									className={`${styles.FooterLogo} ${styles.Invert}`}
+									src="/steam.png"
+								/>
+							</a>
+						</span>
+					</div>
 				</div>
-			</div>
-		</VoteContext.Provider>
+			</VoteContext.Provider>
+		</>
 	);
 }
