4e217eaf02a8eabe536a319c6742ac3b0ba00f13
rett gerst <tricagamma@gmail.com>
2021-04-14 16:12:27 -0400

    tweaks after testing on a real iphone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+import smoothscroll from 'smoothscroll-polyfill';
+
+if (process.browser) {
+	smoothscroll.polyfill();
+}
+
 import './styles.scss';
 
 const queryClient = new QueryClient();
